963fb4ab061e0c6c97b2591a88046cacc769f1a5
mongtamquoc2015@gmail.com <namisan@DESKTOP-2TGMPR1.localdomain>
2020-10-09 08:51:01 +0700

    Modules

--- src/containers/jumbotron.js
+++ src/containers/jumbotron.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Jumbotron from '../components/Jumbotron';
 import jumboData from '../fixtures/jumbo.json';
+import { Jumbotron } from '../components';
 
 function JumbotronContainer() {
 	return (
